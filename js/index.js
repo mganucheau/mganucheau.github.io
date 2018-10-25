35516da8d2ebba1f39249affd834719158e9b1b8
Matt Ganucheau <mganucheau@gmail.com>
2018-10-25 10:26:13 -0700

    editing updates

--- js/index.js
+++ js/index.js
@@ -1,30 +1,46 @@
-    $(document).ready(function () {
-        $("#about").click(function(){
-            $("#include").load("/include/about.html"); 
-        });
-        
-        $("#fmf").click(function(){
-            $("#include").load("/include/fmf.html"); 
-        });
-        $("#clickclean").click(function(){
-            $("#include").load("/include/clickclean.html"); 
-        });
-        
-        $("#pe3").click(function(){
-            $("#include").load("/include/pe3.html"); 
-        });
+$(document).ready(function () {
+    $("#include").load("/include/about.html"); 
 
-        $("#cv").click(function(){
-            $("#include").load("/include/cv.html"); 
-        });
-                    
-        $("#resume").click(function(){
-            $("#include").load("/include/resume.html"); 
-        });
-
-        $("#seeingsound").click(function(){
-            $("#include").load("/include/seeingsound.html"); 
-        });
-                    
-        
+    $("#about").click(function(){
+        $("#include").load("/include/about.html"); 
     });
+
+    $("#fmf").click(function(){
+        $("#include").load("/include/fmf.html"); 
+    });
+
+    $("#clickclean").click(function(){
+        $("#include").load("/include/clickclean.html"); 
+    });
+
+    $("#pe3").click(function(){
+        $("#include").load("/include/pe3.html"); 
+    });
+
+    $("#cv").click(function(){
+        $("#include").load("/include/cv.html"); 
+    });
+
+    $("#resume").click(function(){
+        $("#include").load("/include/resume.html"); 
+    });
+
+    $("#seeingsound").click(function(){
+        $("#include").load("/include/seeingsound.html"); 
+    });
+
+    $("#oraclear").click(function(){
+        $("#include").load("/include/oraclear.html"); 
+    });
+
+
+});
+//
+//$(window).bind('resize', function(e)
+//{
+//  if (window.RT) clearTimeout(window.RT);
+//  window.RT = setTimeout(function()
+//  {
+//    this.location.reload(false); /* false to get page from cache */
+//  }, 200);
+//});
